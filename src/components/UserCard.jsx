1766c46e175613d6a8325220b83ee98a959304a4
Piyush Bagul <piyushvbagul916@gmail.com>
2023-12-31 18:01:23 +0530

    ..

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,30 +1,35 @@
 import { FaPhoneAlt } from "react-icons/fa";
 
-const UserCard = () => {
+const UserCard = ({ data, currentMonth }) => {
+  console.log(currentMonth);
+  const { NAME, PHONE, STB_ID } = data;
   return (
     <div>
       <div className="bg-[#1D1D1D] p-4 rounded-md shadow-md border-gray-600 my-4 text-[#C3C3C3]">
         <div className="nameStatus flex justify-between">
           <div className="div">
-            <div className="name text-xl font-semibold mb-4">
-              ASHOK SURYAVAUSHI
-            </div>
+            <div className="name text-xl font-semibold mb-4">{NAME}</div>
             <div className="phoneAndSTB flex gap-x-4 text-sm">
-              <div className="phone flex">
+              <div className="phone flex text-xs">
                 <FaPhoneAlt className="my-auto mr-2" />
-                9876543210
+                {PHONE}
               </div>
-              <div className="stb">STB: 3155890494</div>
+              <div className="stb overflow-hidden text-xs">STB: {STB_ID}</div>
             </div>
           </div>
-
-          <div className="unpaid ">
-            <span className="bg-red-500 text-white font-semibold p-2 rounded-lg">
-              {" "}
-              UNPAID
-            </span>
-          </div>
-          {/* <div className="paid">PAID</div> */}
+          {data[currentMonth] === "" ? (
+            <div className="unpaid">
+              <div className="bg-red-500 w-[4.2rem] text-center text-white font-medium text-sm p-2 rounded-lg">
+                UNPAID
+              </div>
+            </div>
+          ) : (
+            <div className="paid">
+              <div className="bg-green-500 w-[4.2rem] text-center  text-white font-medium text-sm p-2 rounded-lg">
+                PAID
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -2,7 +2,42 @@ import UserCard from "../components/userCard";
 import { IoSearch } from "react-icons/io5";
 import { FaChevronDown } from "react-icons/fa";
 
+import { useState, useEffect } from "react";
+// import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const UserList = () => {
+  const [customerData, setCustomerData] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState("");
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const Months = currentDate.toLocaleString("en-US", { month: "long" });
+    setCurrentMonth(Months.toLowerCase());
+  }, []);
+
+  //function to fetch data from db
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      setCustomerData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  //fetch data
+  useEffect(() => {
+    fetchData();
+  }, []);
+  //   console.log(customerData);
+
   return (
     <div>
       <div className="">
@@ -14,25 +49,27 @@ const UserList = () => {
               placeholder="Search..."
             />
             <div className="btn bg-orange-500 flex items-center justify-center p-4 rounded-lg cursor-pointer">
-              <IoSearch className="text-xl" />
+              <IoSearch className="text-xl text-white" />
             </div>
           </div>
         </nav>
         {/* --------------------------------------------- */}
-        <div className="container mt-12 flex justify-between">
+        <div className="container mt-12 flex justify-between item">
           <div className="AllUsers flex gap-x-2">
             <h1 className="text-2xl font-bold">All Users</h1>
             <span className="text-lg font-thin mt-1">(183)</span>
           </div>
 
-          <div className="sort bg-slate-700 p-2 px-3 rounded-lg flex item-center justify-between w-28">
+          <div className="sort bg-slate-700 p-2 px-3 rounded-lg flex item-center justify-between w-28 ">
             Sort By: <FaChevronDown className="ml-2 my-auto" />
           </div>
         </div>
         {/* --------------------------------------------- */}
         <div className="cardContainer mt-8">
-          <UserCard />
-          <UserCard />
+          {customerData.map((data, index) => (
+            <UserCard key={index} data={data} currentMonth={currentMonth} />
+            //   {data[currentMonth] === "" ? <td>UNPAID</td> : <td>PAID</td>}
+          ))}
         </div>
       </div>
     </div>
