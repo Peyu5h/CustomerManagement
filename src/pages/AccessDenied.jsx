84ab443877f2c9b05d00b9981529e722a346ee71
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-02 12:37:56 +0530

    body:margin-o

--- src/pages/AccessDenied.jsx
+++ src/pages/AccessDenied.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const AccessDenied = () => {
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center gap-y-1 p-6">
+    <div className="min-h-screen w-full flex flex-col justify-center items-center gap-y-1 p-6">
       <h1 className="text-8xl font-black">403</h1>
       <h2 className="text-2xl font-semibold text-red-500 mb-1">
         Access Denied
