d11f588b5f60eb2ed081d6e44812d6a74907aadb
Piyush Bagul <piyushvbagul916@gmail.com>
2024-01-04 14:26:16 +0530

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,20 @@ const App = () => {
     <>
       <div className="h-screen w-full ">
         <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/accessDenied" element={<AccessDenied />} />
-
-          {isAuthenticated ? (
-            <>
-              <Route path="/users" element={<UserList />} />
-              <Route path="/user/:id" element={<UserDetail />} />
-              <Route path="/addUser" element={<AddUser />} />
-              <Route path="/deleteUser" element={<DeleteUser />} />
-              <Route path="/exportUser" element={"www.google.com"} />
-            </>
-          ) : (
+          {/* <Route path="/" element={<Auth />} />
+          <Route path="/accessDenied" element={<AccessDenied />} /> */}
+          {/* 
+          {isAuthenticated ? ( */}
+          <>
+            <Route path="/users" element={<UserList />} />
+            <Route path="/user/:id" element={<UserDetail />} />
+            <Route path="/addUser" element={<AddUser />} />
+            <Route path="/deleteUser" element={<DeleteUser />} />
+            <Route path="/exportUser" element={"www.google.com"} />
+          </>
+          {/* ) : (
             <Route path="*" element={<Navigate to="/accessDenied" />} />
-          )}
+          )} */}
         </Routes>
       </div>
     </>
